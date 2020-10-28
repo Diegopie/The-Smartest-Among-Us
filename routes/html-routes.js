38eb2ef9ad496d8c75f642c0f211bdb7535d70a5
TheyKay <murraykyleb@gmail.com>
2020-10-28 17:21:03 -0600

    changed callback functions to fat arrow type

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -3,19 +3,19 @@ const path = require("path");
 // routes
 module.exports = function (app) {
   // index
-  app.get("/", function (req, res) {
+  app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "../public/index.html"));
   });
   // account
-  app.get("/account", function (req, res) {
+  app.get("/account", (req, res) => {
     res.sendFile(path.join(__dirname, "../public/account.html"));
   });
   // play
-  app.get("/play", function (req, res) {
+  app.get("/play", (req, res) => {
     res.sendFile(path.join(__dirname, "../public/play.html"));
   });
   // create
-  app.get("/create", function (req, res) {
+  app.get("/create", (req, res) => {
     res.sendFile(path.join(__dirname, "../public/create.html"));
   });
 };
