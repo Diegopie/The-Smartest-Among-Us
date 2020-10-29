47aa9bbdf74d83c135990b21358e1d000797a187
TheyKay <murraykyleb@gmail.com>
2020-10-28 19:11:49 -0600

    jk, reverted to original html-routes

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -4,18 +4,18 @@ const path = require("path");
 module.exports = function (app) {
   // index
   app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "index.html"));
+    res.sendFile(path.join(__dirname, "../public/index.html"));
   });
   // account
   app.get("/account", (req, res) => {
-    res.sendFile(path.join(__dirname, "account.html"));
+    res.sendFile(path.join(__dirname, "../public/account.html"));
   });
   // play
   app.get("/play", (req, res) => {
-    res.sendFile(path.join(__dirname, "play.html"));
+    res.sendFile(path.join(__dirname, "../public/play.html"));
   });
   // create
   app.get("/create", (req, res) => {
-    res.sendFile(path.join(__dirname, "create.html"));
+    res.sendFile(path.join(__dirname, "../public/create.html"));
   });
 };
