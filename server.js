c3358a595161b1ac75514293f007c4a55cf981f4
Diego Hernandez <ishtaracademy@outlook.com>
2020-10-28 19:58:44 -0600

    Updated pages, added prototype pages to the public folder

--- server.js
+++ server.js
@@ -4,13 +4,16 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 8080;
 
-// app.use(express.static("public"));
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+app.use(express.static("public"));
 
 // app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 // app.set("view engine", "handlebars");
 
 app.listen(PORT, () => {
   console.log("Server listening on: " + PORT);
+  console.log("http://localhost:" + 8080 + "/");
 });
