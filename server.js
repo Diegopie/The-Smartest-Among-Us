7b0bdccc289971e6cb3d2d9d71bda6f7ea064b95
Diego Hernandez <ishtaracademy@outlook.com>
2020-10-28 20:01:51 -0600

    fixed merge conflict

--- server.js
+++ server.js
@@ -4,16 +4,13 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 8080;
 
-
+app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
-app.use(express.static("public"));
 
 // app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 // app.set("view engine", "handlebars");
 
 app.listen(PORT, () => {
   console.log("Server listening on: " + PORT);
-  console.log("http://localhost:" + 8080 + "/");
 });
