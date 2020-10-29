937016439b8a2ae56d903785a51e7d9dbba85e25
TheyKay <murraykyleb@gmail.com>
2020-10-28 19:06:26 -0600

    added line 7 to make public directory the home base thing and updated html-routes to reflect that

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 8080;
 
-// app.use(express.static("public"));
+app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
