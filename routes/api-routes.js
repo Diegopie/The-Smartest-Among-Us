12daf66c15c63afa1ee4bb81cb61c1e7b3a3f039
TheyKay <murraykyleb@gmail.com>
2020-10-30 19:09:31 -0600

    sampleQuiz for quickly populating database for testing, finished (?) api routes

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,11 +1,32 @@
 const db = require("../models");
 
 module.exports = function (app) {
+  // create new user account entry
+  app.post("/api/user", (req, res) => {
+    db.Account.create({
+      username: req.body.username,
+      adminKey: req.body.adminKey,
+    }).then((result) => {
+      res.json(result);
+    });
+  });
+
   // return all quizzes owned by this user
-  app.get("/api/:userId", (req, res) => {
+  app.get("/api/:accountId", (req, res) => {
     db.Quiz.findAll({
       where: {
-        accountId: req.params.userId,
+        accountId: req.params.accountId,
+      },
+    }).then((result) => {
+      res.json(result);
+    });
+  });
+
+  // get quiz from quizID
+  app.get("/api/:quizID", (req, res) => {
+    db.Quiz.findOne({
+      where: {
+        quizID: req.params.quizID,
       },
     }).then((result) => {
       res.json(result);
@@ -15,18 +36,18 @@ module.exports = function (app) {
   // creating a quiz
   // POST data structure from front end
   // const newQuiz = {
-  //    quizName: "whatever",
-  //    randomize: true,
-  //    accountID: 1
-  //    questions: [
+  //   quizName: "whatever",
+  //   randomize: true,
+  //   accountID: 1,
+  //   questions: [
   //     {
-  //       questionNum: ,
+  //       questionNum: 1,
   //       question: "some text",
   //       correctAnswer: "this one's right",
-  //       wrongAnswers: ["wrong", "nope", "git out"]
+  //       wrongAnswers: ["wrong", "nope", "git out"],
   //     },
-  // ]
-  // }
+  //   ],
+  // };
   // creating a quiz
   app.post("/api/quiz", (req, res) => {
     let quizID;
@@ -47,6 +68,30 @@ module.exports = function (app) {
           wrongAnswer3: entry.wrongAnswers[2],
         });
       });
+      res.json(result);
+    });
+  });
+
+  // enter the score
+  app.post("/api/newScore/:quizID", (req, res) => {
+    db.Score.create({
+      username: req.body.username,
+      score: req.body.score,
+      quizID: req.params.quizID,
+    }).then((result) => {
+      res.json(result);
+    });
+  });
+
+  // get scores for specific quiz
+  app.get("api/hiScores/:quizID", (req, res) => {
+    db.Score.findAll({
+      where: {
+        quizID: req.params.quizID,
+      },
+      order: [["score", "DESC"]],
+      limit: 10,
+    }).then((result) => {
       res.json(result);
     });
   });
