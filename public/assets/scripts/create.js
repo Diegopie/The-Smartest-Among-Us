42422a9c1cabc30296ab75d92f2a92a0f0b705bd
Diego Hernandez <ishtaracademy@outlook.com>
2020-11-02 15:13:41 -0700

    centered p elem. in css; create page can now dynamicaly create question containers

--- public/assets/scripts/create.js
+++ public/assets/scripts/create.js
@@ -1,47 +1,79 @@
 // * Global Variables
-// ** Store User Selection for API Req
+// ** Store User Selections
+let strQuestions;
+let randomize = Boolean;
 let userCat;
 let userDif;
-const quizRes = [];
+let quizRes;
 // ** Store API Res
-let randomize = Boolean;
 
 // * Functions
 // ** Test API Res
 function testAPI() {
-  const url = "https://opentdb.com/api.php?amount=10&type=multiple";
+  const url =
+    "https://opentdb.com/api.php?amount=" + strQuestions + "&type=multiple";
   $.ajax({
     url: url,
     method: "GET",
   }).then((res) => {
     // Push res to quizRes then run renderQuiz to update DOM
-    quizRes.push(res.results);
+    quizRes = res.results;
     renderQuiz();
   });
+}
+
+// makeQuesCont(2);
+// ** Create Question Containers
+function makeQuesCont(value) {
+  for (let i = 0; i < value; i++) {
+    // *** Create Elements
+    const qNum = i + 1;
+    const qCont = $("<article>").addClass("cont question");
+    const qTitle = $("<h3>").text("Question " + qNum);
+    const btn = $("<button>").addClass("button q-rmv").text("X");
+    const qInpt = $("<input>").attr({ type: "text", name: "q" + qNum });
+    const corTitle = $("<h3>").text("Correct Answer");
+    const cInpt = $("<input>").attr({
+      type: "text",
+      name: "q" + qNum + "-crt",
+    });
+    const inTitle = $("<h3>").text("Imposter Answers");
+    qCont.append(qTitle, btn, qInpt, corTitle, cInpt, inTitle);
+    for (let j = 1; j < 4; j++) {
+      const iInpt = $("<input>").attr({
+        type: "text",
+        name: "q" + qNum + "-wrg" + j,
+      });
+      qCont.append(iInpt);
+    }
+    // *** Append to DOM
+    $("#quiz-cont").append(qCont);
+  }
 }
 
 // ** Display parsedQuiz to the Screen
 function renderQuiz() {
   // *** Loop through the Length of the quizRes array
-  for (let i = 0; i < quizRes[0].length; i++) {
-    const resPath = quizRes[0][i];
+  for (let i = 0; i < quizRes.length; i++) {
+    const resPath = quizRes[i];
     const domPath = $(".question")[i].children;
-    // console.dir($(".question")[i].children);
+    // console.dir(domPath);
     // *** Update Question
-    domPath[1].value = resPath.question;
+    domPath[2].value = resPath.question;
     // *** Update Correct Answer
-    domPath[3].value = resPath.correct_answer;
+    domPath[4].value = resPath.correct_answer;
     // *** Update Incorrect Answer
-    domPath[5].value = resPath.incorrect_answers[0];
-    domPath[6].value = resPath.incorrect_answers[1];
-    domPath[7].value = resPath.incorrect_answers[2];
+    domPath[6].value = resPath.incorrect_answers[0];
+    domPath[7].value = resPath.incorrect_answers[1];
+    domPath[8].value = resPath.incorrect_answers[2];
   }
   // Show Update DOM Elements
   $("#quiz-cont").removeClass("hide");
   $("#api").removeClass("hide");
 }
-console.dir($("#quiz-cont")[0].children.length);
-// ** Store Questions In Ong for Our API
+
+
+// ** Store Questions In Obj for Our API
 function parseUser() {
   // *** Variables
   // Checks how many question containers are on the dom
@@ -51,18 +83,19 @@ function parseUser() {
   // *** Store DOM Elements
   // Get Quiz Name
   const quizName = $("#qname")[0].value;
-  // Get Account ID??
+  // GET ACCOUNT ID??
   // *** Get All Questions Containers
   // Objects created in for loop will be pushed here
   const questions = [];
   for (let i = 0; i < quizLength; i++) {
     const domPath = $(".question")[i].children;
+    // console.log(domPath);
     // Get Question
-    const question = domPath[1].value;
+    const question = domPath[2].value;
     // Get Correct
-    const correct = domPath[3].value;
+    const correct = domPath[4].value;
     // Get Incorrect
-    const wrong = [domPath[5].value, domPath[6].value, domPath[7].value];
+    const wrong = [domPath[6].value, domPath[7].value, domPath[8].value];
     // Create question object
     const curQuest = {
       questionNum: questionNum,
@@ -82,10 +115,20 @@ function parseUser() {
     questions: questions,
   };
   // ONCE we are server connected, we can pass this object to an API Req
+  console.log("-- Obj for API --");
   console.log(apiObj);
 }
 
 // * Click Listeners
+// ** Store How Many Questions To Render
+$("#amnt-sbt").click((event) => {
+  event.preventDefault();
+  strQuestions = parseInt($("#amnt-val").val());
+  console.log("How Many Q: " + strQuestions);
+  $("#amnt").addClass("hide");
+  $("#rand").removeClass("hide");
+  makeQuesCont(strQuestions);
+});
 // ** Set Randomize to True
 $("#randyes").click((event) => {
   event.preventDefault();
@@ -117,13 +160,13 @@ $("#autono").click((event) => {
 $(".cat").click((event) => {
   event.preventDefault();
   userCat = event.target.innerText;
-  console.log(userCat);
+  console.log("User Category: ", userCat);
 });
 // ** Store User Difficulty
 $(".dif").click((event) => {
   event.preventDefault();
   userDif = event.target.innerText;
-  console.log(userDif);
+  console.log("User Difficulty: ", userDif);
 });
 // Make Trivia API Req
 $("#sub").click((event) => {
@@ -135,4 +178,10 @@ $("#sub").click((event) => {
 $("#api").click((event) => {
   event.preventDefault();
   parseUser();
+});
+
+$(".q-rmv").click((event) => {
+  event.preventDefault();
+  // console.dir(event.target.parentElement);
+  event.target.parentElement.remove();
 });
