d7436615d403ad48701f12d1b3c3f345cbdeb805
Slothing <seth.r.martineau@gmail.com>
2020-11-01 21:53:18 -0700

    prettier issues

--- models/user.js
+++ models/user.js
@@ -10,13 +10,13 @@ module.exports = function (sequelize, DataTypes) {
       unique: true,
       validate: {
         isEmail: true,
-      }
+      },
     },
     // The password cannot be null
     password: {
       type: DataTypes.STRING,
       allowNull: false,
-    }
+    },
   });
   // Creating a custom method for our User model. This will check if an unhashed password entered by the user can be compared to the hashed password stored in our database
   User.prototype.validPassword = function (password) {
