3f374c7d45c7c4226dc5a1fd47a017be5d0207b5
Diego Hernandez <ishtaracademy@outlook.com>
2020-11-03 20:01:39 -0700

    Validating user selections working, updated CSS

--- public/assets/scripts/create.js
+++ public/assets/scripts/create.js
@@ -1,5 +1,8 @@
 $(() => {
   // * Global Variables
+  // ** Used to Check if User Clicked an API Setting Button
+  let catSelect = false;
+  let difSelect = false;
   // ** Checks If Validator Ran
   let valCheck = Boolean;
   // ** Store User Selections
@@ -37,7 +40,7 @@ $(() => {
       // Contain X Button
       const xCont = $("<article>").addClass("col-12");
       const xDiv = $("<div>").addClass("row");
-      const btn = $("<button>").addClass("q-rmv").text("X");
+      const btn = $("<button>").addClass("button q-rmv").text("X");
       xDiv.append(btn);
       xCont.append(xDiv);
       // Contain Question
@@ -79,27 +82,24 @@ $(() => {
     }
     $(".q-rmv").click((event) => {
       event.preventDefault();
-      console.log(event.target.parentElement.parentElement.parentElement);
+      // console.log(event.target.parentElement.parentElement.parentElement);
       event.target.parentElement.parentElement.parentElement.remove();
       updateQuesTitles();
     });
   }
 
-  // ** Test API Res
-  // function testAPI() {
-  //   const url =
-  //     "https://opentdb.com/api.php?amount=" + strQuestions + "&type=multiple";
-  //   // "https://opentdb.com/api.php?amount=" + 2 + "&type=multiple";
-  //   $.ajax({
-  //     url: url,
-  //     method: "GET",
-  //   }).then((res) => {
-  //     // Push res to quizRes then run renderQuiz to update DOM
-  //     quizRes = res.results;
-  //     renderQuiz();
-  //   });
-  // }
+  // ** Adds Class When User Click and API Option and Removes Class For Any Previous Click
+  function userSelect(elem) {
+    const path = elem.parentElement.children;
+    for (let i = 0; i < path.length; i++) {
+      if ($(path[i]).hasClass("selected")) {
+        $(path[i]).removeClass("selected");
+      }
+    }
+    $(elem).addClass("selected");
+  }
 
+  // ** Make API Req Directly to Their Site
   function testSelect() {
     const url =
       "https://opentdb.com/api.php?amount=" +
@@ -109,32 +109,16 @@ $(() => {
       "&difficulty=" +
       difValue[userDif] +
       "&type=multiple";
-    console.log(url);
+
     $.ajax({
       url: url,
       method: "GET",
     }).then((res) => {
-      console.log(res);
-      console.log(res.results);
       quizRes = res.results;
       renderQuiz();
     });
   }
 
-  // ** Test API Res
-  // function testAPI() {
-  //   const url =
-  //     "https://opentdb.com/api.php?amount=" + strQuestions + "&type=multiple";
-  //   // "https://opentdb.com/api.php?amount=" + 2 + "&type=multiple";
-  //   $.ajax({
-  //     url: url,
-  //     method: "GET",
-  //   }).then((res) => {
-  //     // Push res to quizRes then run renderQuiz to update DOM
-  //     quizRes = res.results;
-  //     renderQuiz();
-  //   });
-  // }
   // ** Display API Res to DOM (This depends on children elements. Any adding additional elements to the makeQuestCont() will break this!)
   function renderQuiz() {
     // *** Loop through the Length of the quizRes array
@@ -142,7 +126,6 @@ $(() => {
       // ** Variables
       const resPath = quizRes[i];
       const domPath = $(".question")[i].children;
-      // console.dir(domPath);
       // *** Update Question
       domPath[1].children[1].children[0].value = resPath.question;
       // *** Update Correct Answer
@@ -166,9 +149,6 @@ $(() => {
     // *** Loop Through the Amount of Question Containers Currently on the DOM and Update Them From 1
     for (let i = 0; i < quizLength; i++) {
       const domPath = $(".question")[i].children[1].children[0];
-      console.log(domPath);
-      // console.log(domPath);
-      // console.log(domPath[0].textContent);
       domPath.textContent = "Question " + questionNum;
       questionNum++;
     }
@@ -185,7 +165,7 @@ $(() => {
       $(elem).removeClass("valid");
     }
   }
-  // console.dir($(".question")[0].children);
+
   // ** Store Questions In Obj for Our API
   function parseUser() {
     // *** Variables
@@ -210,14 +190,11 @@ $(() => {
     const questions = [];
     for (let i = 0; i < quizLength; i++) {
       const domPath = $(".question")[i].children;
-      // console.log(domPath);
       // Get Question
       const question = domPath[1].children[1].children[0];
-      console.log("question: ", question);
       validator(question);
       // Get Correct
       const correct = domPath[2].children[1].children[0];
-      console.log("Correct: ", correct);
       validator(correct);
       // Get Incorrect
       validator(domPath[3].children[1].children[0]);
@@ -252,10 +229,9 @@ $(() => {
     };
     // ONCE WE ARE SERVER CONNECTED, WE CAN PASS THIS OBJECT TO AN API REQ
     $.post("/api/quiz", apiObj).then(() => {
-      console.log("work??");
+      console.log("-- Obj for API --");
+      console.log(apiObj);
     });
-    console.log("-- Obj for API --");
-    console.log(apiObj);
   }
 
   // * Click Listeners
@@ -268,6 +244,7 @@ $(() => {
     $("#rand").removeClass("hide");
     makeQuesCont(strQuestions);
   });
+
   // ** Set Randomize to True
   $("#randyes").click((event) => {
     event.preventDefault();
@@ -275,6 +252,7 @@ $(() => {
     $("#auto").removeClass("hide");
     randomize = true;
   });
+
   // ** Set Randomize to False
   $("#randno").click((event) => {
     event.preventDefault();
@@ -282,12 +260,14 @@ $(() => {
     $("#auto").removeClass("hide");
     randomize = false;
   });
+
   // ** Display Catagories on Yes
   $("#autoyes").click((event) => {
     event.preventDefault();
     $("#auto").addClass("hide");
     $("#settings").removeClass("hide");
   });
+
   // ** Hide Buttons and Display Questions Containers on No
   $("#autono").click((event) => {
     event.preventDefault();
@@ -295,31 +275,44 @@ $(() => {
     $("#quiz-cont").removeClass("hide");
     $("#btns").removeClass("hide");
   });
+
   // ** Store the Category A User Clicks
   $(".cat").click((event) => {
     event.preventDefault();
     userCat = event.target.innerText;
+    userSelect(event.target);
+    catSelect = true;
     console.log("User Category: ", userCat);
   });
+
   // ** Store User Difficulty
   $(".dif").click((event) => {
     event.preventDefault();
     userDif = event.target.innerText;
+    userSelect(event.target);
+    difSelect = true;
     console.log("User Difficulty: ", userDif);
   });
-  // Make Trivia API Req
+
+  //** Make Trivia API Req
   $("#sub").click((event) => {
     event.preventDefault();
+    if (!catSelect || !difSelect) {
+      window.alert("Choose Something, fool");
+      return;
+    }
     $("#settings").addClass("hide");
     // testAPI();
     testSelect();
   });
-  // Make Server API Req
+
+  // ** Make Server API Post Req
   $("#api").click((event) => {
     event.preventDefault();
     parseUser();
   });
-  // Add A New Question Container
+
+  // ** Add A New Question Container
   $("#new-btn").click((event) => {
     event.preventDefault();
     makeQuesCont(1);
