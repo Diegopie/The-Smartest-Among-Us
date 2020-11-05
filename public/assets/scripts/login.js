060e6f63e9e9b15313bdd74402d0c7ff5ff7b354
TheyKay <murraykyleb@gmail.com>
2020-11-04 17:40:39 -0700

    redirect user to their account page after successful login

--- public/assets/scripts/login.js
+++ public/assets/scripts/login.js
@@ -32,8 +32,9 @@ $(document).ready(() => {
       .then((res) => {
         console.log(res.id);
         localStorage.setItem("currentUserId", res.id);
+        const accountURL = "/account=" + email;
         console.log(localStorage.getItem("currentUserId"));
-        window.location.replace("/account");
+        window.location.replace(accountURL);
       })
       .catch((err) => {
         console.log(err);
